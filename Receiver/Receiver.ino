d47a3bf9e5be63297b780bd177ca3d84686daa6c
Arshaque Mohammed <mohd.arshaque@gmail.com>
2017-10-28 20:03:53 +0530

    Add files via upload

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,55 @@
+#include<LiquidCrystal.h>  
+#include<IRremote.h>
+
+//IR
+int RECV_PIN = 10;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+//LCD
+int Contrast=80;
+int BackLight = 30;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);   
+
+//Buzzer
+int speakerPin = 13;
+
+
+ void setup()
+ { 
+    analogWrite(6,Contrast);
+     analogWrite(9,BackLight);
+     lcd.begin(16, 2);
+     Serial.begin(9600);
+  irrecv.enableIRIn();
+     pinMode(speakerPin,OUTPUT);
+  }  
+  
+  void buzz(){
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+      digitalWrite(speakerPin, HIGH);
+      delay(200);
+      digitalWrite(speakerPin,LOW);
+      delay(50);
+  }
+     delay(2000);
+  }
+  
+  
+  
+  
+     void loop()
+ { 
+     lcd.setCursor(0, 0);
+     lcd.print("Have a safe ride!");
+     if(irrecv.decode(&results))
+    {
+      lcd.clear();
+      Serial.println(results.value, HEX);
+      irrecv.resume();
+      lcd.setCursor(0, 0);
+     lcd.print("Side Please!");
+     buzz();
+    }
+     delay(300);
+}
---
+++ Sender/Sender.ino
@@ -0,0 +1,48 @@
+// Joystick x- and y- axes are connected to A1 and A0 analog pins of Arduino.
+// Servos are connectd to PWM Pins 9 and 10.
+
+//#include <IRremote.h>
+#include <Servo.h>
+
+
+//unsigned int m[] = {1,2,3,4};
+//IRsend irsend;
+Servo ser; 
+int irPin = 3;
+int irGnd = 4;
+const int buttonPin = 7;
+int buttonState = 0; 
+int joyX = A1; //  X - axis of Joystick
+//int joyY = A0; // y - axis of Joystick
+int x=0;//,y=0; 
+
+void setup()
+{ 
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+  pinMode(irPin, OUTPUT);
+  pinMode(irGnd, OUTPUT);
+  ser.attach(9); // servo on PIN 9
+} 
+
+void sendIRS(){
+  digitalWrite(irPin,HIGH);
+   digitalWrite(irGnd,LOW);
+   delay(100);
+   digitalWrite(irPin,LOW);
+}
+
+void loop()
+{ 
+  x = joyX;    // y value btwn 0 and 1023) 
+  x = map(analogRead(joyX), 0, 1024, 90, 1);
+  ser.write(x); 
+  buttonState = digitalRead(buttonPin);
+  //irsend.sendRaw(m,4, 12); 
+  Serial.println(x);
+  //Serial.println(sw1);
+  Serial.println(buttonState);
+ if(buttonState == LOW){
+   sendIRS();
+ }
+}
